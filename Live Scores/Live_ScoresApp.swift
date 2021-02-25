3d0070689ca994366e7587067f29b9829c1e0e55
Zuhair Ali <zuhairali20003@gmail.com>
2021-02-25 10:24:17 -0600

    Initial Commit

---
+++ Live Scores/Live_ScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_ScoresApp.swift
+//  Live Scores
+//
+//  Created by Student on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Live_ScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
